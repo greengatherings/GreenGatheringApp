263015cd751d0e56451180a076676aa87695a882
Vineet Agarwal <vineet.agarwal@nxp.com>
2025-01-31 16:44:24 +0530

    Added code to switch the pages via anchor tags

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link, Routes, Route, BrowserRouter, MemoryRouter } from 'react-router-dom'; // Import the Link component
+import { Link, Routes, Route, BrowserRouter, MemoryRouter, Navigate } from 'react-router-dom'; // Import the Link component
 import ReUseBooksApp from './ReUseBooks/ReUseBooks';
+import ReactDOM from 'react-dom';
 const App: React.FC = () => {
     return (
         <div >
@@ -9,13 +10,16 @@ const App: React.FC = () => {
             <div >
             <MemoryRouter >
                 <nav>
-                    <Link to="/">Home</Link>
-                    <br/>
-                    <Link to="/ReUseBooksApp">ReUse School Books Initiative</Link>
+                    <table><tr>
+                    
+                    <td><Link to="/">Home</Link></td>
+                    
+                    <td><Link to="/reusebooks">ReUse School Books</Link></td>
+                    </tr></table>
                     </nav>
                 <Routes>
                     <Route path="/" element={<HomeApp/>}/>
-                    <Route path="/ReUseBooksApp" element={<ReUseBooksApp />} />
+                    <Route path="/reusebooks" element={<ReUseBooksApp />} />
                 </Routes>
              </MemoryRouter>
             </div>
@@ -25,12 +29,32 @@ const App: React.FC = () => {
     );
 };
 
+
+function reUseBook_click(){ 
+    ReactDOM.render(
+        <>
+        <ReturnToHome/>
+        <ReUseBooksApp/>
+        </>,
+        document.getElementById('root')
+    );
+}
+
+const ReturnToHome: React.FC = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+        </div>
+    );
+}
+
+
 const HomeApp: React.FC = () => {
     return (
         <div>
             <div style={{ textAlign: 'center', backgroundColor: '#f0f8ff', padding: '20px' }}>
                 <h1 style={{ color: 'green', fontWeight: 'bold', fontSize: '2.5em', margin: '20px 0' }}>
-                    Welcome the Green Gatherings
+                    Welcome to Green Gatherings
                 </h1>
             </div>
             <hr />
@@ -38,6 +62,9 @@ const HomeApp: React.FC = () => {
                 <p style={{  fontSize: '2.5em', margin: '20px 0' }}>
                     Green Gatherings is encouraging green initiatives that help conserve our environment.
                 </p>
+                <br/>
+                <a style={{fontSize: '2em'}} href="javascript:void(0)" onClick={reUseBook_click}>ReUseBooks Initiative</a>
+                
             </div>
         </div>
     );
--- src/components/ReUseBooks/ReUseBooks.tsx
+++ src/components/ReUseBooks/ReUseBooks.tsx
@@ -9,14 +9,14 @@ const ReUseBooksApp: React.FC = () => {
             <hr />
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <img src="/images/books.png" alt="Books for all" style={{ width: '300px', height: 'auto', marginRight: '20px' }} />
-                <div style={{ textAlign: 'center' }}>
+                <div style={{ textAlign: 'left' }}>
                     <p style={{ fontSize: '1.3em' }}>It is an initiative by Green Gatherings to provide a platform for donating and receiving used textbooks. Your contribution can make a significant impact to conserve environment. 
-                        How it works:
-                        1. User Fills the form as Donor and./or Receiver of School books.
-                        2. Once the match is found, the receiver will get the contact details of donor over an email.
-                        3. Receiver is then expected to contact the donor for fixing time and place to collect the books.
-                        4. Once the books are exchanged, receiver is expected to send and email confirming the collection mentioning donors Name
-                        5. System is updated according to close the requirements.
+                    <br/><b>How it works:</b><br/>
+                        1. User Fills the form as Donor and./or Receiver of School books.<br/>
+                        2. Once the match is found, the receiver will get the contact details of donor over an email.<br/>
+                        3. Receiver is then expected to contact the donor for fixing time and place to collect the books.<br/>
+                        4. Once the books are exchanged, receiver is expected to send and email confirming the collection mentioning donors Name<br/>
+                        5. System is updated according to close the requirements.<br/>
                     </p>
                     <p style={{ fontSize: '1.3em' }}>We are thrilled to announce the successful completion of the ReUse School Books 2024! With over 600 requirements registered, we are proud to report that 60% of participants were able to find a perfect match for their needs. This remarkable achievement highlights the power of community and the importance of sustainability in education.</p>
                     <br />
